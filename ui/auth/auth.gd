901a462bbfe4d88da5dddb9ce18d1b2453d9c1d6
Virágh Balázs <97942621+balyy@users.noreply.github.com>
2024-12-06 22:14:56 +0100

    Updated auth systems
    I wanna kms

--- ui/auth/auth.gd
+++ ui/auth/auth.gd
@@ -1,39 +1,38 @@
 extends Control
 
 func _ready():
-	pass
+	Supabase.auth.connect("error", Callable(self, "_on_auth_error"))
 
 
-func _on_sign_up_button_pressed() -> void:
-	var email = %EmailLineEdit.text
-	var password = %PasswordLineEdit.text
-	Supabase.auth.sign_up(email, password)
-	
+func _on_auth_error(error: Object):
+	print("Authentication error: ", error)
+	%StateLabel.text = error.to_string()
+
+
+
+func sign_in():
+	if not Supabase.auth.is_connected("signed_in", Callable(self, "_on_signed_in")):
+		Supabase.auth.connect("signed_in", Callable(self, "_on_signed_in"))
+	Supabase.auth.sign_in(%EmailLineEdit.text, %PasswordLineEdit.text)
+
+func _on_signed_in(user : SupabaseUser):
+	print("Successfully signed as ", user)
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func sign_up():
+	if not Supabase.auth.is_connected("signed_in", Callable(self, "on_signed_in")):
+		Supabase.auth.connect("signed_up", Callable(self, "on_signed_up"))
+	Supabase.auth.sign_up(%EmailLineEdit.text, %PasswordLineEdit.text)
+
+func on_signed_up(user : SupabaseUser):
+	print("Succesfully signed up as ", user)
 
 
 func _on_login_button_pressed() -> void:
-	var email = %EmailLineEdit.text
-	var password = %PasswordLineEdit.text
-	Supabase.auth.sign_in(email, password)
-	if Supabase.auth.sign_in(email, password).completed:
-		get_tree().change_scene_to_file("res://main.tscn")
+	print("login")
+	sign_in()
 
 
-func on_error():
-	pass
-
-
-func on_login_succeded(auth):
-	pass
-
-
-func on_login_failed(auth):
-	pass
-
-
-func on_signup_succeded(auth):
-	pass
-
-
-func on_signup_failed(auth):
-	pass
+func _on_sign_up_button_pressed() -> void:
+	print("register")
+	sign_up()
