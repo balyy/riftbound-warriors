72659ce6fe141f31ffcc09ad58f0dee5bcdfc63c
Robby <schindler.robert_2020c@bolyai-szakkozep.hu>
2025-04-14 01:31:34 +0200

    beta main menu
    bug fix / upgrade WIP

--- main.gd
+++ main.gd
@@ -1,1 +1,24 @@
-extends Node2D
+extends Control
+
+@export var game_scene: PackedScene
+@export var auth_scene: PackedScene
+
+func _ready():
+	$PlayButton.text = "Játék indítása"
+	$AuthButton.text = "Bejelentkezés"
+	$QuitButton.text = "Kilépés"
+
+	$PlayButton.pressed.connect(_on_play_pressed)
+	$AuthButton.pressed.connect(_on_auth_pressed)
+	$QuitButton.pressed.connect(_on_quit_pressed)
+
+func _on_play_pressed():
+	if game_scene:
+		get_tree().change_scene_to_packed(game_scene)
+
+func _on_auth_pressed():
+	if auth_scene:
+		get_tree().change_scene_to_packed(auth_scene)
+
+func _on_quit_pressed():
+	get_tree().quit()
