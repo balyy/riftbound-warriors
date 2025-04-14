e72316288ae02fe11eccdfc94d79e181d4f96b42
Robby <schindler.robert_2020c@bolyai-szakkozep.hu>
2025-04-14 11:00:14 +0200

    Menu fix
    bug fix WIP

--- main.gd
+++ main.gd
@@ -2,19 +2,26 @@ extends Control
 
 @export var game_scene: PackedScene
 @export var auth_scene: PackedScene
+@export var leaderboard_scene: PackedScene
 
 func _ready():
-	$PlayButton.text = "Játék indítása"
-	$AuthButton.text = "Bejelentkezés"
-	$QuitButton.text = "Kilépés"
+	$PanelContainer/MarginContainer/VBoxContainer/PlayButton.text = "Start game"
+	$PanelContainer/MarginContainer/VBoxContainer/LeaderboardButton.text = "Leaderboard"
+	$PanelContainer/MarginContainer/VBoxContainer/AuthButton.text = "Login"
+	$PanelContainer/MarginContainer/VBoxContainer/QuitButton.text = "Quit"
 
-	$PlayButton.pressed.connect(_on_play_pressed)
-	$AuthButton.pressed.connect(_on_auth_pressed)
-	$QuitButton.pressed.connect(_on_quit_pressed)
+	$PanelContainer/MarginContainer/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
+	$PanelContainer/MarginContainer/VBoxContainer/LeaderboardButton.pressed.connect(_on_leaderboard_pressed)
+	$PanelContainer/MarginContainer/VBoxContainer/AuthButton.pressed.connect(_on_auth_pressed)
+	$PanelContainer/MarginContainer/VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
 
 func _on_play_pressed():
 	if game_scene:
 		get_tree().change_scene_to_packed(game_scene)
+
+func _on_leaderboard_pressed():
+	if leaderboard_scene:
+		get_tree().change_scene_to_packed(leaderboard_scene)
 
 func _on_auth_pressed():
 	if auth_scene:
